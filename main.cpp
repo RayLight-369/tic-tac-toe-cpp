cb687720e4c2796cd4b9e226b15e75b802bbc5f6
Abdul Rafay <136804809+RayLight-369@users.noreply.github.com>
2024-05-26 22:10:55 +0500

    updating files

--- main.cpp
+++ main.cpp
@@ -8,17 +8,21 @@ void drawBoard()
 
   system("cls");
 
-  cout << "   |   |   " << endl;
-  cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << "  \n";
-  cout << "   |   |   " << endl;
-  cout << "-----------" << endl;
-  cout << "   |   |   " << endl;
-  cout << " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << "  \n";
-  cout << "   |   |   " << endl;
-  cout << "-----------" << endl;
-  cout << "   |   |   " << endl;
-  cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << "  \n";
-  cout << "   |   |   " << endl;
+  cout << "     |     |     " << endl;
+  cout << "  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] << endl;
+
+  cout << "_____|_____|_____" << endl;
+  cout << "     |     |     " << endl;
+
+  cout << "  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << endl;
+
+  cout << "_____|_____|_____" << endl;
+  cout << "     |     |     " << endl;
+
+  cout << "  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << endl;
+
+  cout << "     |     |     " << endl
+       << endl;
 };
 
 bool addMark(const int &cell, const char &mark)
